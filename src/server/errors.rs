14041862626ab1cb071265cde6497982a7475e51
YourCarma <y0urcarma@yandex.ru>
2026-03-27 16:45:40 +0300

    Errors for server

--- src/server/errors.rs
+++ src/server/errors.rs
@@ -16,9 +16,9 @@ pub type ServerResult<T> = Result<T, ServerError>;
 
 #[derive(Debug, Error, Serialize, ToSchema)]
 pub enum ServerError {
-    #[error("not found error: {0}")]
+    #[error("Not found error: {0}")]
     NotFound(String),
-    #[error("Provider is unavailable: {0}")]
+    #[error("Broker is unavailable: {0}")]
     ServiceUnavailable(String),
     #[error("Unauthorized request: {0}")]
     Unauthorized(String),
@@ -26,37 +26,27 @@ pub enum ServerError {
     DeserializeError(String),
     #[error("API key has no credits: {0}")]
     NoCredits(String),
-    #[error("Model requires a moderation {0}")]
-    ModelModerationError(String),
     #[error("User is limited: {0}")]
     RateLimited(String),
-    #[error("Invalid Model Response: {0}")]
-    InvalidResponse(String),
     #[error("IO Error")]
     IOError(String),
     #[error("Timeout Error")]
     Timeout(String),
-    #[error("Bad request")]
-    BadRequest(String),
     #[error("Internal server error: {0}")]
     InternalError(String),
     #[error("Serde error: {0}")]
     SerdeError(String),
     #[error("Reqeust error: {0}")]
     RequestError(String),
-    #[error("Content Too large: {0}")]
-    ContentTooLarge(String),
 }
 
 impl ServerError {
     pub fn status_code(&self) -> (String, StatusCode) {
         match self {
             ServerError::NotFound(msg) => (msg.to_owned(), StatusCode::NOT_FOUND),
-            ServerError::BadRequest(msg) => (msg.to_owned(), StatusCode::BAD_REQUEST),
             ServerError::DeserializeError(msg) => (msg.to_owned(), StatusCode::BAD_GATEWAY),
             ServerError::IOError(msg) => (msg.to_owned(), StatusCode::NO_CONTENT),
             ServerError::RequestError(msg) => (msg.to_owned(), StatusCode::BAD_GATEWAY),
-            ServerError::InvalidResponse(msg) => (msg.to_owned(), StatusCode::NO_CONTENT),
             ServerError::NoCredits(msg) => (msg.to_owned(), StatusCode::PAYMENT_REQUIRED),
             ServerError::RateLimited(msg) => (msg.to_owned(), StatusCode::TOO_MANY_REQUESTS),
             ServerError::Timeout(msg) => (msg.to_owned(), StatusCode::REQUEST_TIMEOUT),
@@ -65,46 +55,24 @@ impl ServerError {
             }
             ServerError::Unauthorized(msg) => (msg.to_owned(), StatusCode::UNAUTHORIZED),
             ServerError::InternalError(msg) => (msg.to_owned(), StatusCode::INTERNAL_SERVER_ERROR),
-            ServerError::ModelModerationError(msg) => (msg.to_owned(), StatusCode::FORBIDDEN),
             ServerError::SerdeError(msg) => (msg.to_owned(), StatusCode::UNPROCESSABLE_ENTITY),
-            ServerError::ContentTooLarge(msg) => (msg.to_owned(), StatusCode::PAYLOAD_TOO_LARGE),
         }
     }
 }
 
-impl From<FromUtf8Error> for ServerError {
-    fn from(err: FromUtf8Error) -> Self {
-        tracing::error!("From UTF8 Error: {err}", err = err.to_string());
-        Self::BadRequest("Decoding error".to_string())
-    }
-}
 
-impl From<MultipartError> for ServerError {
-    fn from(err: MultipartError) -> Self {
-        tracing::error!("Multipart Error: {err}", err = err.body_text());
-        Self::BadRequest("Decoding Error of ContentToLarge".to_string())
-    }
-}
-
-impl From<TryFromSliceError> for ServerError {
-    fn from(err: TryFromSliceError) -> Self {
-        tracing::error!("Error decoding UiserID!: {err}", err = err.to_string());
-        Self::BadRequest("Decoding error".to_string())
-    }
-}
-
-impl From<std::io::Error> for ServerError {
-    fn from(err: std::io::Error) -> Self {
-        tracing::error!("Error: {err}", err = err.to_string());
-        Self::BadRequest("Error saving file".to_string())
-    }
-}
 
 impl From<PublisherErrors> for ServerError {
     fn from(err: PublisherErrors) -> Self {
         tracing::error!("Error: {err}", err = err.to_string());
         match err {
-            PublisherErrors::BadRequest(err) => ServerError::BadRequest(err.to_string()),
+            PublisherErrors::DeserializeError(err) => Self::SerdeError(err.to_string()),
+            PublisherErrors::NotFoundError(err) => Self::NotFound(err.to_string()),
+            PublisherErrors::Unauthorized(err) => Self::Unauthorized(err.to_string()),
+            PublisherErrors::IOError(err) => Self::IOError(err.to_string()),
+            PublisherErrors::ServiceUnavailable(err) => Self::ServiceUnavailable(err.to_string()),
+            PublisherErrors::SerializeError(err) => Self::SerdeError(err.to_string()),
+            PublisherErrors::AnotherError(err) => Self::InternalError(err.to_string())
            
         }
     }
